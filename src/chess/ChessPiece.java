f50d8dd50331a43131c85c9c47ed76ba8ac7611a
Ilson Warmling <ilsonwarsc@gmail.com>
2022-07-24 21:08:11 -0300

    Special move - Castling

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -27,7 +27,7 @@ public abstract class ChessPiece extends Piece {
         moveCount++;
     }
 
-    public void decraseMoveCount() {
+    public void decreaseMoveCount() {
         moveCount--;
     }
 
